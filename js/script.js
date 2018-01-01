efacb4c097ddbd8f5f0208a4053957a02dc33b77
Karolina Siwkowska <trallala00@gmail.com>
2018-01-01 13:23:02 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var i, a;
+
+for (i = 1; i <= 6; i++) {
+    for (a = 1; a <= i; a++) {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+var i, a;
+
+for (i = 1; i <= 8; i++) {
+    for (a = 1; a <= i; a++) {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+var i, a;
+
+for (i = 1; i <= 10; i++) {
+    for (a = 1; a <= i; a++) {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+document.write("<br>");
